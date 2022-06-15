ded629259c8e667e14e460067b19be952557f6c6
samir bouzidi <fouleck30@hotmail.fr>
2022-06-15 10:54:31 +0200

    Mode error handle done

--- srcs/cmds/mode_command.cpp
+++ srcs/cmds/mode_command.cpp
@@ -1,11 +1,50 @@
 #include "../../ft_irc.hpp"
 
+Client					*getClientByname(Server *server, std::string name)
+{
+	std::map<int, Client*>::iterator it = server->client.begin();
+	std::map<int, Client*>::iterator ite = server->client.end();
+
+	for (; it != ite; it++)
+	{
+		if ((*it).second->getNickname() == name)
+			return (*it).second;
+	}
+	return (nullptr);
+}
+
+bool mode_parser(std::vector<std::string> cmd, Client *client_target, int clientFd, Server *server) {
+
+    std::string mode = client_target->getMode();
+    bool clear;
+    std::string options = "+-aiwro";
+
+    for (size_t i = 0; i != cmd.size() - 1; ++i)
+    {
+        if (options.find(cmd[2][i]) == '-')
+				clear = true;
+		else if (options.find(cmd[2][i]) == '+')
+				clear = false;
+        if (options.find(cmd[2][i]) == std::string::npos)
+		{
+            send_error("502", server->client[clientFd]->getNickname(), "Unknown MODE flag", clientFd);
+            return false;
+        }
+
+
+
+    }
+
+    return true;
+}
+
+
+
 bool ModeForUser(std::vector<std::string> cmd, int clientFd, Server *server) {
 
     Client *client_target = NULL;
     std::string options = "+-aiwro";
-    //int clear = 0;
-
+    
     if (cmd[1] == server->client[clientFd]->getNickname())
 		client_target = server->client[clientFd];
     else
@@ -15,11 +54,15 @@ bool ModeForUser(std::vector<std::string> cmd, int clientFd, Server *server) {
             send_error("502", server->client[clientFd]->getNickname(), "Cant change mode for other users", clientFd);
             return false;
         }
-        //client_target = cmd.getSender()->getServ()->getUserByname(cmd.getArgs()[0]);
+        client_target = getClientByname(server, cmd[1]);
 		if (!client_target)
-			return false;
-	}
-    
+		{
+            send_error_1("401", server->client[clientFd]->getNickname(), "No such nick/channel", clientFd, cmd[1]);
+            return false;
+        }
+    }
+    if (!mode_parser(cmd, client_target, clientFd, server))
+        return false;
 
     return true;
 }
