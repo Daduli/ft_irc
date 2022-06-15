32abc5416c51cb243389667509bf417b4d084f92
samir bouzidi <fouleck30@hotmail.fr>
2022-06-15 11:32:38 +0200

    mode looks good

--- srcs/cmds/mode_command.cpp
+++ srcs/cmds/mode_command.cpp
@@ -16,29 +16,47 @@ Client					*getClientByname(Server *server, std::string name)
 bool mode_parser(std::vector<std::string> cmd, Client *client_target, int clientFd, Server *server) {
 
     std::string mode = client_target->getMode();
-    bool clear;
+    bool clear = false;
     std::string options = "+-aiwro";
 
     for (size_t i = 0; i != cmd.size() - 1; ++i)
     {
-        if (options.find(cmd[2][i]) == '-')
-				clear = true;
-		else if (options.find(cmd[2][i]) == '+')
+        if (cmd[2][i] == '-')
+		{
+                printf("suppression");       
+                clear = true;
+        }
+		else if (cmd[2][i] == '+')
 				clear = false;
         if (options.find(cmd[2][i]) == std::string::npos)
 		{
-            send_error("502", server->client[clientFd]->getNickname(), "Unknown MODE flag", clientFd);
+            send_error("501", server->client[clientFd]->getNickname(), "Unknown MODE flag", clientFd);
             return false;
         }
-
-
-
+        else
+        {
+            if (clear == true && (&mode)->find(cmd[2][i]) != std::string::npos)
+		    {
+                printf("suppression");
+                if (cmd[2][i] != '+' && cmd[2][i] != '-')
+                {
+                    (&mode)->erase((&mode)->find(cmd[2][i]), 1);
+                    server->client[clientFd]->setMode(mode);
+                }
+            }
+            else if (clear == false && (&mode)->find(cmd[2][i]) == std::string::npos)
+            {
+                if (cmd[2][i] != '+' && cmd[2][i] != '-')
+                {
+                    (&mode)->push_back(cmd[2][i]);
+                    server->client[clientFd]->setMode(mode);
+                }
+            }
+        }
     }
-
+    sendMode("221", server->client[clientFd]->getNickname(), clientFd, server->client[clientFd]->getMode());
     return true;
 }
-
-
 
 bool ModeForUser(std::vector<std::string> cmd, int clientFd, Server *server) {
 
