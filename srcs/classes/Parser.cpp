ac700486877078e5918ae9088715497d92c2e6fa
samir bouzidi <fouleck30@hotmail.fr>
2022-06-17 10:07:38 +0200

    topic work with irssi:

--- srcs/classes/Parser.cpp
+++ srcs/classes/Parser.cpp
@@ -95,7 +95,7 @@ void	Parser::getCommand(std::vector<std::string> cmd, int clientFd, Server *serv
 		mode_command(cmd, clientFd, server);
 	else if (_command == "PRIVMSG")
 		privmsg_command(cmd, clientFd, server);
-	else if (_command == "TOPIC") //fix to work with irssi
+	else if (_command == "TOPIC") 
 		topic_command(cmd, clientFd, server);
 	else if (_command == "NOTICE")
 		notice_command(cmd, clientFd, server);
